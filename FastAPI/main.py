70eaf51f7ff6af16ae0a0fd24f520df855b9b58d
Rohang Shah <rohangshah1003@gmail.com>
2025-12-29 22:47:39 +0530

    main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from database import connect_to_mongo, close_mongo_connection
-from routes import router
+from routes import router as user_router
 from app.api.resume import router as resume_router
 
 app = FastAPI(title="FastAPI MongoDB Integration")
 
-app.include_router(resume_router, prefix="/api/resume", tags=["Resume"])
+app.include_router(resume_router, prefix="/api", tags=["Resume"])
 
 @app.on_event("startup")
 async def startup_event():
@@ -15,7 +15,7 @@ async def startup_event():
 async def shutdown_event():
     await close_mongo_connection()
 
-app.include_router(router, prefix="/api", tags=["users"])
+app.include_router(user_router, prefix="/api", tags=["users"])
 
 @app.get("/")
 async def root():
